a16cc9ea63df7701b2071085ba49daaebdbf068b
rinziv <sax@host-131-114-44-4.m.unipi.it>
2017-04-03 14:53:56 +0200

    New organization of the code with functions

--- lines/assets/js/main.js
+++ lines/assets/js/main.js
@@ -1,44 +1,50 @@
 // get a new dataset
-var numbers = [];
 
-var n = Math.round(Math.random()*15);
-
-
-
-numbers = d3.range(n).map(function(d){
-	return Math.round(Math.random()*250);
-});
 
 //
 // for (var i = 0; i < n; i++) {
 // 	var r = Math.round(Math.random()*250);
 // 	numbers.push(r)
 // }
-
 	
-
-
-
-
-var svg = d3.select("#viz")
-	.append("svg")
-	.attr("width",600)
-.attr("height",400);
+function generateNumbers(){
+	var numbers = [];
+	var n = Math.round(Math.random()*15);
 	
-var gs = svg.selectAll("g")
-	.data(numbers)
-	.enter()
-	.append("g")
-	.attr("transform",function(d,i){
-		return "translate(10,"+(i*14+10)+")";
+	numbers = d3.range(n).map(function(d){
+		return Math.round(Math.random()*250);
 	});
 	
-gs.append("line")
-	.attr("x2",function(d){return d})
-	.attr("stroke","black")
-	;
+	return numbers;
+}
+
+
+function visualizeNumbers(myNumbers){
+	var svg = d3.select("#viz")
+		.append("svg")
+		.attr("width",600)
+	.attr("height",400);
 	
-gs.append("text")
-	.attr("x",function(d){return d + 4})
-	.text(function(d){return d});
+	var gs = svg.selectAll("g")
+		.data(myNumbers)
+		.enter()
+		.append("g")
+		.attr("transform",function(d,i){
+			return "translate(10,"+(i*14+10)+")";
+		});
+	
+	gs.append("line")
+		.attr("x2",function(d){return d})
+		.attr("stroke","black")
+		;
+	
+	gs.append("text")
+		.attr("x",function(d){return d + 4})
+		.text(function(d){return d});
+}
+
+
+var numbers = generateNumbers();
+console.log("numbers",numbers);
+visualizeNumbers(numbers);
 	
