ac572d9d8ad86bed48f1ea3a0a058bf93559645e
rinziv <sax@host-131-114-44-4.m.unipi.it>
2017-04-03 15:25:17 +0200

    Handler of exit() and update() and transition

--- lines/assets/js/main.js
+++ lines/assets/js/main.js
@@ -7,6 +7,11 @@
 // 	numbers.push(r)
 // }
 	
+var svg = d3.select("#viz")
+	.append("svg")
+	.attr("width",600)
+.attr("height",400);	
+	
 function generateNumbers(){
 	var numbers = [];
 	var n = Math.round(Math.random()*15);
@@ -14,37 +19,51 @@ function generateNumbers(){
 	numbers = d3.range(n).map(function(d){
 		return Math.round(Math.random()*250);
 	});
-	
+	console.log("numbers",numbers);
 	return numbers;
 }
 
 
 function visualizeNumbers(myNumbers){
-	var svg = d3.select("#viz")
-		.append("svg")
-		.attr("width",600)
-	.attr("height",400);
-	
 	var gs = svg.selectAll("g")
-		.data(myNumbers)
+	.data(myNumbers);
+	
+	// exit
+	var gsToRemove = gs.exit().remove();
+	
+	// append
+	var gsToAdd = gs
 		.enter()
 		.append("g")
 		.attr("transform",function(d,i){
 			return "translate(10,"+(i*14+10)+")";
 		});
-	
-	gs.append("line")
+	gsToAdd.append("line").attr("x2",0).attr("stroke","black");
+	gsToAdd.append("text").attr("x",0);	
+		
+	// update
+	gs.select("line")
+		.transition()
 		.attr("x2",function(d){return d})
-		.attr("stroke","black")
+		.duration(1500)
 		;
 	
-	gs.append("text")
+	gs.select("text")
+		.transition()
 		.attr("x",function(d){return d + 4})
+		.duration(1500)
 		.text(function(d){return d});
 }
 
 
 var numbers = generateNumbers();
-console.log("numbers",numbers);
+
 visualizeNumbers(numbers);
+
+d3.select("#shuffle")
+	.on("click", function(d){
+		console.log("click");
+		visualizeNumbers(generateNumbers());
+	})
+
 	
