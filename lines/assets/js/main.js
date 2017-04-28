e37273b52f51407d72948a7943262062d5c3acb8
rinziv <sax@host-131-114-41-115.m.unipi.it>
2017-04-28 15:41:39 +0200

    New version with modules

--- lines/assets/js/main.js
+++ lines/assets/js/main.js
@@ -1,32 +1,107 @@
-// get a new dataset
 
-
-//
-// for (var i = 0; i < n; i++) {
-// 	var r = Math.round(Math.random()*250);
-// 	numbers.push(r)
-// }
-
-
-// General setting of the enviroment
-//   * create an SVG container
-//   * prepare an axis
+var myLineChart = lineBarChart();
 var svg = d3.select("#viz")
 	.append("svg")
 	.attr("width",600)
-.attr("height",400);	
+.attr("height",400);
 
-var xAxis = d3.svg.axis()
-.orient("top");
+svg.datum(generateNumbers()).call(myLineChart);
 
-svg.append("g")
-	.attr("class","axis")
-    .attr("transform","translate(10,40)")
-.call(xAxis);
 
-var gnumbers = svg.append("g")
-    .attr("class","numbers")
-.attr("transform","translate(10,60)");
+function lineBarChart(){
+	var width = 500,
+		height =400;
+	
+	function me(selection){
+		// let's assume selection is an SVG container
+		console.log("selection", selection.datum());
+		
+		var xAxis = d3.svg.axis()
+		.orient("top");
+
+		if(selection.select("g.axis").empty()){
+			selection.append("g")
+				.attr("class","axis")
+			    .attr("transform","translate(10,40)")
+			.call(xAxis);
+		}
+		
+		var gnumbers = selection.select("g.numbers");
+		if(gnumbers.empty()){
+			gnumbers = selection.append("g")
+			    .attr("class","numbers")
+			.attr("transform","translate(10,60)");
+		}
+		
+		
+		var gs = gnumbers.selectAll("g.number")
+		.data(selection.datum());
+	
+		// exit
+		var gsToRemove = gs.exit().remove();
+	
+		// append
+		var gsToAdd = gs
+			.enter()
+			.append("g")
+			.attr("class","number")
+			.attr("transform",function(d,i){
+				return "translate(0,"+(i*24+10)+")";
+			});
+		gsToAdd.append("line").attr("x2",0).attr("stroke","black");
+		gsToAdd.append("text")
+			.attr("text-anchor","end")
+			.attr("dy",-2)
+			.attr("x",0);	
+		
+		// update
+		var scale = d3.scale.linear()
+			.domain([0,d3.max(selection.datum())])
+		.range([0,580]);
+	
+		xAxis.scale(scale);
+    
+	    selection.select("g.axis")
+	    .call(xAxis);
+	
+		// var cScale = d3.scale.linear()
+		// 	.domain([0,d3.max(myNumbers)])
+		// .range(["#fff", "#f00"]);
+		var cScale = d3.scale.quantile()
+			.domain(selection.datum())
+		.range(colorbrewer['Purples'][4]);
+		//.range(["#fee0d2","#fc9272","#de2d26"]);  // Reds-3classes
+	
+
+	
+		gs.select("line")
+			.transition()
+			.attr("x2",scale)
+			.attr("stroke",cScale)
+			.duration(1500)
+			;
+	
+		gs.select("text")
+			.transition()
+			.attr("x",function(d){return scale(d)})
+			.duration(1500)
+			.text(function(d){return d});
+	}
+	
+	me.width = function(_){
+		if(!arguments.length) return width;
+		width = _;
+		return me;
+	}
+	
+	me.height = function(_){
+		if(!arguments.length) return height;
+		height = _;
+		return me;
+	}
+	
+	return me;
+}
 	
     
 // Random generator of integers
@@ -43,58 +118,7 @@ function generateNumbers(){
 
 
 function visualizeNumbers(myNumbers){
-	var gs = gnumbers.selectAll("g.number")
-	.data(myNumbers.sort());
 	
-	// exit
-	var gsToRemove = gs.exit().remove();
-	
-	// append
-	var gsToAdd = gs
-		.enter()
-		.append("g")
-		.attr("class","number")
-		.attr("transform",function(d,i){
-			return "translate(0,"+(i*24+10)+")";
-		});
-	gsToAdd.append("line").attr("x2",0).attr("stroke","black");
-	gsToAdd.append("text")
-		.attr("text-anchor","end")
-		.attr("dy",-2)
-		.attr("x",0);	
-		
-	// update
-	var scale = d3.scale.linear()
-		.domain([0,d3.max(myNumbers)])
-	.range([0,580]);
-	
-	xAxis.scale(scale);
-    
-    svg.select("g.axis")
-    .call(xAxis);
-	
-	// var cScale = d3.scale.linear()
-	// 	.domain([0,d3.max(myNumbers)])
-	// .range(["#fff", "#f00"]);
-	var cScale = d3.scale.quantile()
-		.domain(myNumbers)
-	.range(colorbrewer['Purples'][4]);
-	//.range(["#fee0d2","#fc9272","#de2d26"]);  // Reds-3classes
-	
-
-	
-	gs.select("line")
-		.transition()
-		.attr("x2",scale)
-		.attr("stroke",cScale)
-		.duration(1500)
-		;
-	
-	gs.select("text")
-		.transition()
-		.attr("x",function(d){return scale(d)})
-		.duration(1500)
-		.text(function(d){return d});
 }
 
 
@@ -105,7 +129,7 @@ visualizeNumbers(numbers);
 d3.select("#shuffle")
 	.on("click", function(d){
 		console.log("click");
-		visualizeNumbers(generateNumbers());
+		svg.datum(generateNumbers()).call(myLineChart);
 	})
 
 	
