7becee91b058c13363579bad0b59ab4171df7c99
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2017-05-07 22:51:49 +0200

    Internal refactoring with utility function to group data

--- geo-dipinti/assets/js/charts.js
+++ geo-dipinti/assets/js/charts.js
@@ -2,17 +2,12 @@
 
 function Chart(){
 	var dimension = "Technique";
+	var museum;
 	var svg;
 	var nvchart;
 	
 	function me(selection){
 		console.log("paintings", selection.datum());
-		
-		// group data
-		var grouped = d3.nest()
-			.key(function(d){return d["TECHNIQUE"]})
-			.rollup(function(leaves){return leaves.length})
-		.entries(selection.datum());
 		
 		if(!svg){
 			selection.append("h4")
@@ -21,18 +16,38 @@ function Chart(){
 			svg = selection.append("svg")
 			.attr({width:"100%", height:400});
 			
-			nvchart = nv.models.multiBarHorizontalChart();
+			nvchart = nv.models.multiBarHorizontalChart()
+				.margin({left:100})
+				.showLegend(false)
+				.showControls(false);
 		}
-		svg.datum([
-			{
-				key:"Count " + dimension,
-				values: grouped
-					.map(function(d){return {x:d.key, y:d.values}})
-			}
-		])
+		svg.datum(groupDataBy(selection.datum(),dimension))
 		.call(nvchart);
 	}
 	
+	function groupDataBy(data, dimension, museum){
+		var filtered;
+		if(!museum){
+			filtered = data;
+		}else{
+			filtered = data.filter(function(d){return m.MUSEUM==museum})
+		}
+		
+		var grouped = d3.nest()
+			.key(function(d){return d[dimension.toUpperCase()].toLowerCase()})
+			.rollup(function(leaves){return leaves.length})
+		.entries(filtered);
+		
+		return [
+			{
+				key:"Count " + dimension,
+				values: grouped
+					.map(function(d){return {x:d.key, y:d.values}}) // rename proerty name of objects
+					.sort(function(a,b){return -a.y + b.y}) // sort by frequency
+					.filter(function(d,i){return i < 10})  // select only first 10 rows
+			}
+		]
+	}
 	
 	return me;
 }
