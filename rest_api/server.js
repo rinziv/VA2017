7d40430768fc8b3718b6098cf2dfcd99c5b329fe
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2017-05-14 16:30:04 +0200

    Code refactoring

--- rest_api/server.js
+++ rest_api/server.js
@@ -10,6 +10,13 @@ var port = process.env.PORT || 5000;
 var router = express.Router();  // Router associate URL requests to specific handlers
 var dsv = d3.dsv(";","text/html")
 
+var file_URL = "assets/data/opere_colori.csv";
+var opere;
+
+
+function loadAndParseData(filepath){
+	
+}
 
 // define a test route to check if the server is working correctly
 router.get('/', function(req, res){
@@ -18,30 +25,28 @@ router.get('/', function(req, res){
 
 router.route('/painting')
 	.get(function(req, res){
-		fs.readFile("assets/data/opere_colori.csv", "utf-8", function(error, opere){
-			if(error) res.send(error);
+		fs.readFile(file_URL, "utf-8", function(error, data){
+			if(error) throw error;
+	
+			opere = dsv.parse(data);
 			
-			data = dsv.parse(opere);
-			
-			res.json(data);
-			
-			
+			res.json(opere);
 		})
 	});
 	
 
 router.route('/painting/byauthor/:author')
 	.get(function(req, res){
-		fs.readFile("assets/data/opere_colori.csv", "utf-8", function(error, opere){
-			if(error) res.send(error);
+		fs.readFile(file_URL, "utf-8", function(error, data){
+			if(error) throw error;
+	
+			opere = dsv.parse(data);
 			
-			data = dsv.parse(opere);
-			
-			res.json(data.filter(function(d){
-				return d["AUTHOR"].toLowerCase().indexOf(req.params.author.toLowerCase()) >= 0
-			}));
+			res.json(opere.filter(function(d){
+					return d["AUTHOR"].toLowerCase().indexOf(req.params.author.toLowerCase()) >= 0
+				}));
 		})
-	})
+	});
 
 
 
