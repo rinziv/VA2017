c7e5b3169f3312c713b4e49f7a29de093530b7fe
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2017-05-14 16:08:42 +0200

    Loading data from file and return to client

--- rest_api/server.js
+++ rest_api/server.js
@@ -1,12 +1,14 @@
 // call to required packages
 var express = require('express');
 var d3 = require('d3');
+var fs = require('fs');
 
 var app = express(); // create the application module
 
 var port = process.env.PORT || 5000;
 
 var router = express.Router();  // Router associate URL requests to specific handlers
+var dsv = d3.dsv(";","text/html")
 
 
 // define a test route to check if the server is working correctly
@@ -14,7 +16,19 @@ router.get('/', function(req, res){
 	res.json({message: 'Hello World!'});
 })
 
-
+router.route('/painting')
+	.get(function(req, res){
+		fs.readFile("assets/data/opere_colori.csv", "utf-8", function(error, opere){
+			if(error) res.send(error);
+			
+			data = dsv.parse(opere);
+			
+			res.json(data);
+			
+			
+		})
+	});
+	
 // define a prefix for all the URLs
 app.use('/api', router);
 
