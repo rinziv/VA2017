114cab1d5d8ab2e139773dbdcf0a09a87ec68f14
rinziv <sax@host-131-114-243-13.m.unipi.it>
2017-05-15 14:12:13 +0200

    New subproject for layouts

---
+++ layout/assets/js/cluster.js
@@ -0,0 +1,52 @@
+
+var width = 750,
+    height = 2200;
+
+// var cluster = d3.layout.cluster()
+//     .size([height, width - 160]);
+
+var tree = d3.layout.tree()
+    .size([height, width - 160]);
+
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+var svg = d3.select("#viz").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(40,0)");
+
+d3.json("assets/data/flare.json", function(error, root) {
+  if (error) throw error;
+
+  console.log("root",root);
+
+  // var nodes = cluster.nodes(root),
+  //     links = cluster.links(nodes);
+	  
+  var nodes = tree.nodes(root),
+      links = tree.links(nodes);
+
+	  console.log("links", links);
+  var link = svg.selectAll(".link")
+      .data(links)
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", diagonal);
+
+  var node = svg.selectAll(".node")
+      .data(nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+  node.append("circle")
+      .attr("r", 4.5);
+
+  node.append("text")
+      .attr("dx", function(d) { return d.children ? -8 : 8; })
+      .attr("dy", 3)
+      .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+      .text(function(d) { return d.name; });
+});
